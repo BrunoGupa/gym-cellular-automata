d718813835fea0d0b12ddc0dbb1fd1166f697fdd
Bruno Gutiérrez De La Paz <bruno.gupa@gmail.com>
2021-08-25 16:54:20 -0500

    Update __init__.py
    We add two directions of the new environments, and we add the chapter's name where we find each one. 
    This additional direction name could work better when we have more environments.

--- gym_cellular_automata/__init__.py
+++ gym_cellular_automata/__init__.py
@@ -19,18 +19,37 @@ PROJECT_PATH = Path(__file__).parents[1]
 REGISTERED_CA_ENVS = (
     "ForestFireHelicopter-v0",
     "ForestFireBulldozer-v1",
+    "GoLBomber-v0",
+    "Influencer-v0"
 )
 
 try:
-    ffdir = "gym_cellular_automata.forest_fire"
+    ffdir = "gym_cellular_automata"
     register(
         id=REGISTERED_CA_ENVS[0],
-        entry_point=ffdir + ".helicopter:ForestFireEnvHelicopterV0",
+        entry_point=ffdir + ".forest_fire.helicopter:ForestFireEnvHelicopterV0",
+        max_episode_steps=999,
     )
 
     register(
         id=REGISTERED_CA_ENVS[1],
-        entry_point=ffdir + ".bulldozer:ForestFireEnvBulldozerV1",
+        entry_point=ffdir + ".forest_fire.bulldozer:ForestFireEnvBulldozerV1",
+        max_episode_steps=4096,
+        reward_threshold=0.0,
+    )
+
+    register(
+        id=REGISTERED_CA_ENVS[2],
+        entry_point=ffdir + ".game_of_life.bomber:GoLEnvBomberV0",
+        max_episode_steps=100,
+        #reward_threshold=1000,
+    )
+
+    register(
+        id=REGISTERED_CA_ENVS[3],
+        entry_point=ffdir + ".opinion_dynamics.simple_model:InfluencerEnvV0",
+        max_episode_steps=100,
+        #reward_threshold=1000,
     )
 except GymError:  # Avoid annoying Re-register error when working interactively.
     pass
