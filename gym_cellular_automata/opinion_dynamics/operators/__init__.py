4ed781880f37e54f39e010cf8b510b47b0372934
Bruno Gutiérrez De La Paz <bruno.gupa@gmail.com>
2021-08-30 22:07:01 -0500

    Add files via upload

---
+++ gym_cellular_automata/opinion_dynamics/operators/__init__.py
@@ -0,0 +1,6 @@
+from gym_cellular_automata.opinion_dynamics.operators.ca_opinion_dynamics import OpinionCA
+from gym_cellular_automata.opinion_dynamics.operators.move_modify import (
+    Modify,
+    Move,
+    MoveModify,
+)
